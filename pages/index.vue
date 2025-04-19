282862b67cb9c6fa742dd46ff87c014000f1d92a
Егор Свердлов <bibosos228777@gmail.com>
2025-04-19 15:23:39 +0300

    feat: move header to layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,6 @@
 <template>
   <main class="max-w-md flex flex-col">
-    <header class="flex items-center gap-4">
-      <div class="flex items-center">
-        <img src="/assets/img/logo.png" width="59" height="34">
-        <h2 class="font-bold text-2xl">Koala</h2>
-      </div>
-      <div class="flex items-center">
-        <img src="/assets/img/fire.gif" width="40" height="40"/>
-        <p class="font-semibold">5 дней</p>
-      </div>
-      <UAvatar src="https://github.com/benjamincanac.png" />
-    </header>
+    
     <div class="bg-[#2E2E2E80] p-4 rounded-md">
       <h3>Ваша коала</h3>
       <p class="flex justify-between color-ui-success"><span>Знания</span> <span>600xp</span></p>
