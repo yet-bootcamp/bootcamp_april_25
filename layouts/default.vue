4bdc65578856b9e98b1fe0758750a00e7201ece5
Егор Свердлов <bibosos228777@gmail.com>
2025-04-19 15:53:51 +0300

    feat: practice page

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,7 @@
     <header ref="headerRef" class="flex items-center gap-4 w-full p-6 fixed top-0 left-0">
       <NuxtLink to="/" class="flex items-center">
         <img src="/assets/img/logo.png" width="59" height="34">
-        <h2 class="font-bold text-2xl">Koala</h2>
+        <h2 class="font-bold text-2xl">{{ title }}</h2>
       </NuxtLink>
       <div class="flex items-center ml-auto">
         <img src="/assets/img/fire.gif" width="40" height="40"/>
@@ -11,7 +11,7 @@
       </div>
       <UAvatar src="https://github.com/benjamincanac.png" />
     </header>
-    <main class="container mx-auto p-4" :style="{ marginTop: headerHeight + 'px' }">
+    <main class="container mx-auto p-4" :style="{ marginTop: `${headerHeight}px` }">
       <slot />
     </main>
   </div>
@@ -24,6 +24,12 @@ const headerRef= ref<HTMLElement>()
 const headerHeight = computed(() => {
   if (!headerRef.value) return 0
   return headerRef.value.offsetHeight
+})
+
+const route = useRoute()
+
+const title = computed(() => {
+  return route.meta.title || 'Koala'
 })
 </script>
 
--- pages/practise.vue
+++
@@ -1,9 +0,0 @@
-<script lang="ts" setup>
-
-</script>
-
-<template>
-    <div>
-        <h1>Практика</h1>
-    </div>
-</template>
